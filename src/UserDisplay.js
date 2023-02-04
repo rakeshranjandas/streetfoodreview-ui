fe0e63fc2861661fb850d49c0b3b51a9aad98407
Rakesh <rakesh.das@mindfiresolutions.com>
2023-02-04 16:52:52 +0530

    Routes for Reviews and Friends

--- src/UserDisplay.js
+++ src/UserDisplay.js
@@ -1,28 +1,31 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Reviews from './Reviews'
 import Friends from './Friends'
+import { Route, Routes, Link, useResolvedPath, useMatch } from 'react-router-dom'
 
 export default function UserDisplay() {
 
-    const [curDisplay, setCurDisplay] = useState('reviews')
-    
-    function setCurDisplayToReviews() {
-      setCurDisplay('reviews')
-    }
-
-    function setCurDisplayToFriends() {
-      setCurDisplay('friends')
-    }
-
   return (
-    <div>
+    <div className="user-display">
         <ul>
-            <li onClick={ setCurDisplayToReviews }> Reviews </li>
-            <li onClick={ setCurDisplayToFriends }> Friends </li>
+            <CustomLink to="/reviews"> Reviews </CustomLink>
+            <CustomLink to="/friends"> Friends </CustomLink>
         </ul>
 
-        { curDisplay === 'reviews' ? <Reviews /> : <Friends /> }
+
+        <Routes>
+          <Route path="/" element={<Reviews />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/friends" element={<Friends />} />
+        </Routes>
 
     </div>
   )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return <li className={isActive?'active':''}><Link to={to}> {children} </Link></li>;
 }
