5777279dfc00884d641a7ab7b0d588a02378a7f2
Rakesh <rakesh.das@mindfiresolutions.com>
2023-02-04 15:58:56 +0530

    Reviews and Friends components

--- src/UserDisplay.js
+++ src/UserDisplay.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Reviews from './Reviews'
+import Friends from './Friends'
 
 export default function UserDisplay() {
+
+    const [curDisplay, setCurDisplay] = useState('reviews')
+    
+    function setCurDisplayToReviews() {
+      setCurDisplay('reviews')
+    }
+
+    function setCurDisplayToFriends() {
+      setCurDisplay('friends')
+    }
+
   return (
-    <div>Hello</div>
+    <div>
+        <ul>
+            <li onClick={ setCurDisplayToReviews }> Reviews </li>
+            <li onClick={ setCurDisplayToFriends }> Friends </li>
+        </ul>
+
+        { curDisplay === 'reviews' ? <Reviews /> : <Friends /> }
+
+    </div>
   )
 }
