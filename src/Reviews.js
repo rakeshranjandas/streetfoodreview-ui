ce67c77659dd9ea19f557a19df14f2d592f3c639
Rakesh <rakesh.das@mindfiresolutions.com>
2023-02-05 16:39:04 +0530

    List and Map View radio

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react"
+import ReviewsList from "./ReviewsList"
+import ReviewsMap from "./ReviewsMap"
 
 export default function Reviews() {
+  const [currentView, setCurrentView] = React.useState("list")
+
+  function setViewToList() {
+    setCurrentView("list")
+  }
+
+  function setViewToMap() {
+    setCurrentView("map")
+  }
+
   return (
-    <div>Reviews</div>
-  )
+    <div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            onClick={setViewToList}
+            name="reviews-view"
+            checked={currentView === "list"}
+          />
+          List
+        </label>
+        <label>
+          <input
+            type="radio"
+            onClick={setViewToMap}
+            name="reviews-view"
+            checked={currentView === "map"}
+          />
+          Map
+        </label>
+      </div>
+
+      <div>{currentView === "list" ? <ReviewsList /> : <ReviewsMap />}</div>
+    </div>
+  );
 }
---
+++ src/ReviewsMap.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReviewsMap() {
+  return (
+    <div>ReviewsMap</div>
+  )
+}
