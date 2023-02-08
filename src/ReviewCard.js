e132641dab95e0faa30c1429a203a3f6b3ac1016
Rakesh <rakesh.das@mindfiresolutions.com>
2023-02-08 22:40:43 +0530

    UI change in ReviewCard, Fix warning - alt in user img

--- src/ReviewCard.js
+++ src/ReviewCard.js
@@ -4,13 +4,15 @@ export default function ReviewCard(props) {
   return (
     <div className="box">
       <p className="has-text-weight-bold">{props.review.shopName}</p>
-      <p className="has-text-weight-light">
-        <span>{props.review.description}</span>
-        <span className="has-text-danger ml-4">
+      <p>
+        <span className="has-text-danger">
           {new Array(parseInt(props.review.rating)).fill(0).map((x) => {
             return <StarRating />
           })}
         </span>
+      </p>
+      <p className="has-text-weight-light">
+        <span>{props.review.description}</span>
       </p>
     </div>
   )
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import profile_picture from './profile_generic.jpeg'
+import React from "react"
+import profile_picture from "./profile_generic.jpeg"
 
 const UserProfile = () => {
   return (
     <div className="user-profile">
-      <img src={profile_picture} />
+      <img src={profile_picture} alt="user" />
     </div>
   )
 }
