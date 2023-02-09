0176863ac5b507a43e1ddb07137531e6d4504195
Rakesh <rakesh.das@mindfiresolutions.com>
2023-02-09 23:53:50 +0530

    Refactored action for closing Modal

--- src/AddReviewModalWithButton.js
+++ src/AddReviewModalWithButton.js
@@ -16,20 +16,34 @@ export default function AddReviewModalWithButton() {
   )
 }
 
+let initializeModalActions = {
+  openModal: function ($el) {
+    $el.classList.add("is-active")
+  },
+
+  closeModal: function ($el) {
+    $el.classList.remove("is-active")
+  },
+
+  closeAllModals: function ($el) {
+    ;(document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      initializeModalActions.closeModal($modal)
+    })
+  },
+}
+
 function initializeModal() {
   // Functions to open and close a modal
   function openModal($el) {
-    $el.classList.add("is-active")
+    initializeModalActions.openModal($el)
   }
 
   function closeModal($el) {
-    $el.classList.remove("is-active")
+    initializeModalActions.closeModal($el)
   }
 
   function closeAllModals() {
-    ;(document.querySelectorAll(".modal") || []).forEach(($modal) => {
-      closeModal($modal)
-    })
+    initializeModalActions.closeAllModals()
   }
 
   // Add a click event on buttons to open a specific modal
